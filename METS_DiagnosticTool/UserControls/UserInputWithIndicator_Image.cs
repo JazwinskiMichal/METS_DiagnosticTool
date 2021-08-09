dd1a339bb61c1f8297c538cf8eb670d6a87cfa6e
Michal Jazwinski <mail@mihow.pl>
2021-08-09 22:08:17 +0200

    0.0.0.1 -> initial release working on User Input with Indicator User Control

---
+++ METS_DiagnosticTool/UserControls/UserInputWithIndicator_Image.cs
@@ -0,0 +1,7 @@
+﻿namespace METS_DiagnosticTool_UI.UserControls
+{
+    class UserInputWithIndicator_Image : System.Windows.Controls.Image
+    {
+        public System.Windows.Media.SolidColorBrush Color { get; set; }
+    }
+}
