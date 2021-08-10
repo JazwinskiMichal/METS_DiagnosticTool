22cc5e053ace06dea984e7f80c4c2a4153514715
Michal Jazwinski <mail@mihow.pl>
2021-08-10 15:59:09 +0200

    0.0.0.1 -> added two color recording svg icon

--- METS_DiagnosticTool/UserControls/UserInputWithIndicator_Image.cs
+++ METS_DiagnosticTool/UserControls/UserInputWithIndicator_Image.cs
@@ -3,5 +3,7 @@
     class UserInputWithIndicator_Image : System.Windows.Controls.Image
     {
         public System.Windows.Media.SolidColorBrush Color { get; set; }
+
+        public System.Windows.Media.SolidColorBrush Color1 { get; set; }
     }
 }
