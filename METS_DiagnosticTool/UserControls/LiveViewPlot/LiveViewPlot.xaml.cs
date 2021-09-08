f4e382b838f247935dd6f470615a582a3c3baced
Michal Jazwinski <mail@mihow.pl>
2021-09-08 13:02:04 +0200

    0.0.0.1 -> got styled item in List Box Live View

--- METS_DiagnosticTool/UserControls/LiveViewPlot/LiveViewPlot.xaml.cs
+++ METS_DiagnosticTool/UserControls/LiveViewPlot/LiveViewPlot.xaml.cs
@@ -3,6 +3,7 @@ using LiveCharts.Configurations;
 using METS_DiagnosticTool_Utilities;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading;
@@ -32,9 +33,12 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
         private string _registeredAt;
         private bool _showPreviousValues = false;
         private string _currentTime;
+        private bool _showCartesianChart = true;
+        private bool _showListBox = false;
         #endregion
 
         #region Public Properties
+        #region Global Public Properties
         public RpcClient rpcClient
         {
             get
@@ -49,16 +53,18 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
                     _rpcClient.PLCVariableLiveViewTriggered += RpcClient_PLCVariableLiveViewTriggered;
             }
         }
+        #endregion
 
+        #region Live View Chart
         public ChartValues<LiveViewDataModel> ChartValues { get; set; }
-        
+
         public Func<double, string> DateTimeFormatter { get; set; }
         public double AxisXStep { get; set; }
         public double AxisXUnit { get; set; }
         public double AxisYStep { get; set; }
         public double AxisYUnit { get; set; }
 
-        public bool IsReading { get; set; }
+        public bool IsReadingCartesianChart { get; set; }
 
         public bool ShowPreviousValues
         {
@@ -67,6 +73,26 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
             {
                 _showPreviousValues = value;
                 OnPropertyChanged("ShowPreviousValues");
+            }
+        }
+
+        public bool ShowCartesianChart
+        {
+            get { return _showCartesianChart; }
+            set
+            {
+                _showCartesianChart = value;
+                OnPropertyChanged("ShowCartesianChart");
+            }
+        }
+
+        public bool ShowListBox
+        {
+            get { return _showListBox; }
+            set
+            {
+                _showListBox = value;
+                OnPropertyChanged("ShowListBox");
             }
         }
 
@@ -163,6 +189,22 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
         }
         #endregion
 
+        #region Live View ListBox
+        private ObservableCollection<LiveViewListBoxDataModel> _lstBoxLiveViewData;
+        public ObservableCollection<LiveViewListBoxDataModel> LstBoxLiveViewData
+        {
+            get { return _lstBoxLiveViewData; }
+            set
+            {
+                if (value != this._lstBoxLiveViewData)
+                    _lstBoxLiveViewData = value;
+                OnPropertyChanged("LstBoxLiveViewData");
+            }
+        }
+        #endregion
+
+        #endregion
+
         #region Constructor
         public LiveViewPlot()
         {
@@ -191,7 +233,7 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
 
             SetAxisXLimits(DateTime.Now);
 
-            IsReading = false;
+            IsReadingCartesianChart = false;
 
             DataContext = this;
         }
@@ -200,7 +242,7 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
         {
             _rpcClient.PLCVariableLiveViewTriggered -= RpcClient_PLCVariableLiveViewTriggered;
             ChartValues.Clear();
-            IsReading = false;
+            IsReadingCartesianChart = false;
             DataContext = null;
         }
         #endregion
@@ -250,7 +292,40 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
                 {
                     // Start Live View Mode Here
                     // Read Data from PLC, based on given configuration and show it on the Plot
-                    Read(variableConfig);
+                    // Based on Variable Type Show either Cartesian Chart or ListBox for string Values
+                    switch (TwincatHelper.GetSymbolType(variableConfig.variableAddress))
+                    {
+                        case TwincatHelper.G_ET_TagType.PLCFloatAndVBSingle:
+                        case TwincatHelper.G_ET_TagType.PLCBooleanAndVBBoolean:
+                        case TwincatHelper.G_ET_TagType.PLCDwordAndVBUint:
+                        case TwincatHelper.G_ET_TagType.PLCIntegerAndVBShort:
+                        case TwincatHelper.G_ET_TagType.PLCDintAndVBInt:
+                        case TwincatHelper.G_ET_TagType.PLCLRealAndVBDouble:
+                        case TwincatHelper.G_ET_TagType.PLCUIntegerAndVBUShort:
+                        case TwincatHelper.G_ET_TagType.PLCByte:
+                            ShowCartesianChart = true;
+                            ShowListBox = false;
+                            ReadCartesianChart(variableConfig);
+                            break;
+                        case TwincatHelper.G_ET_TagType.PLCTime:
+                        case TwincatHelper.G_ET_TagType.PLCEnum:
+                        case TwincatHelper.G_ET_TagType.PLCString:
+                            // Create here some example Data for Live View List Box
+                            LstBoxLiveViewData = new ObservableCollection<LiveViewListBoxDataModel>
+                            {
+                                new LiveViewListBoxDataModel()
+                                {
+                                    TimeStamp = DateTime.Now,
+                                    Value = "jebac niebieskich"
+                                }
+                            };
+
+                            ShowCartesianChart = false;
+                            ShowListBox = true;
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
             catch (Exception ex)
@@ -259,14 +334,14 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
             }
         }
 
-        private void Read(VariableConfig variableConfig)
+        private void ReadCartesianChart(VariableConfig variableConfig)
         {
-            if (IsReading) return;
+            if (IsReadingCartesianChart) return;
 
             //lets keep in memory only the last 1000 records,
             //to keep everything running faster, and that's also exact length of a whole X axis
             const int keepRecords = 1000;
-            IsReading = true;
+            IsReadingCartesianChart = true;
 
             Action<VariableConfig> readFromTread = (_varConfig) =>
             {
@@ -276,49 +351,40 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
                     bool _parsed = false;
                     double _trendOld = 0;
 
-                    while (IsReading)
+                    while (IsReadingCartesianChart)
                     {
                         DateTime now = DateTime.Now;
 
                         // Here find declared PLC Variable and read it according to provided Configuration
                         if (!string.IsNullOrEmpty(_varConfig.variableAddress))
                         {
+                            // Parsing the PLC Variable Type, actually jusdt checking is it Boolean or other Numeric Type
+                            _parsed = double.TryParse(TwincatHelper.ReadPLCValues(_varConfig.variableAddress, true), out double _result1);
+
+                            if (_parsed)
+                            {
+                                _trend = _result1;
+
+                                _boolean = false;
+                            }
+                            else
+                            {
+                                TwincatHelper.G_ET_TagType _symbolType = TwincatHelper.GetSymbolType(_varConfig.variableAddress);
+
+                                switch (_symbolType)
+                                {
+                                    case TwincatHelper.G_ET_TagType.PLCBooleanAndVBBoolean:
+                                        _trend = bool.Parse(TwincatHelper.ReadPLCValues(_varConfig.variableAddress, true)) ? 1 : 0;
+                                        _boolean = true;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+
                             switch (_varConfig.loggingType)
                             {
                                 case LoggingType.Polling:
-                                    // HERE NEEDS TO BE PARSING ACCORDING TO VARIABLE TYPE
-                                    _parsed = double.TryParse(TwincatHelper.ReadPLCValues(_varConfig.variableAddress, true), out double _result);
-
-                                    if (_parsed)
-                                    {
-                                        _trend = _result;
-
-                                        _boolean = false;
-                                    }
-                                    else
-                                    {
-                                        TwincatHelper.G_ET_TagType _symbolType = TwincatHelper.GetSymbolType(_varConfig.variableAddress);
-
-                                        switch (_symbolType)
-                                        {
-                                            case TwincatHelper.G_ET_TagType.PLCBooleanAndVBBoolean:
-                                                _trend = bool.Parse(TwincatHelper.ReadPLCValues(_varConfig.variableAddress, true)) ? 1 : 0;
-                                                _boolean = true;
-                                                break;
-
-                                            case TwincatHelper.G_ET_TagType.PLCString:
-                                                break;
-                                            case TwincatHelper.G_ET_TagType.PLCTime:
-                                                break;
-                                            case TwincatHelper.G_ET_TagType.PLCEnum:
-                                                break;
-                                            case TwincatHelper.G_ET_TagType.None:
-                                                break;
-                                            default:
-                                                break;
-                                        }
-                                    }
-
                                     // For Polling dont Show Previous Values
                                     ShowPreviousValues = false;
 
@@ -331,7 +397,10 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
                                     });
 
                                     SetAxisXLimits(now);
-                                    //SetAxisYLimits(_trend);
+                                    if (_trend < 0)
+                                        AxisYMin = _trend;
+                                    else
+                                        AxisYMin = 0;
 
                                     if (ChartValues.Count > keepRecords - 1) ChartValues.RemoveAt(0);
                                     Count = ChartValues.Count;
@@ -342,40 +411,7 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
                                     break;
 
                                 case LoggingType.OnChange:
-                                    // HERE NEEDS TO BE PARSING ACCORDING TO VARIABLE TYPE
-                                    _parsed = double.TryParse(TwincatHelper.ReadPLCValues(_varConfig.variableAddress, true), out double _result1);
-
-                                    if (_parsed)
-                                    {
-                                        _trend = _result1;
-
-                                        _boolean = false;
-                                    }
-                                    else
-                                    {
-                                        TwincatHelper.G_ET_TagType _symbolType = TwincatHelper.GetSymbolType(_varConfig.variableAddress);
-
-                                        switch (_symbolType)
-                                        {
-                                            case TwincatHelper.G_ET_TagType.PLCBooleanAndVBBoolean:
-                                                _trend = bool.Parse(TwincatHelper.ReadPLCValues(_varConfig.variableAddress, true)) ? 1 : 0;
-                                                _boolean = true;
-                                                break;
-
-                                            case TwincatHelper.G_ET_TagType.PLCString:
-                                                break;
-                                            case TwincatHelper.G_ET_TagType.PLCTime:
-                                                break;
-                                            case TwincatHelper.G_ET_TagType.PLCEnum:
-                                                break;
-                                            case TwincatHelper.G_ET_TagType.None:
-                                                break;
-                                            default:
-                                                break;
-                                        }
-                                    }
-
-                                    if ((_trend != _trendOld) /*|| (_trendOld == 0 && _trend == 0)*/)
+                                    if (_trend != _trendOld)
                                     {
                                         now = DateTime.Now;
 
@@ -445,8 +481,48 @@ namespace METS_DiagnosticTool_UI.UserControls.LiveViewPlot
             if (PropertyChanged != null)
                 PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
+    }
 
-        
+    public class LiveViewListBoxDataModel : INotifyPropertyChanged
+    {
+        private DateTime _timeStamp = DateTime.Now;
+        public DateTime TimeStamp
+        {
+            get { return _timeStamp; }
+            set
+            {
+                if (value != this._timeStamp)
+                    _timeStamp = value;
+                OnPropertyChanged("TimeStamp");
+            }
+        }
+
+        private string _value = string.Empty;
+        public string Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value != this._value)
+                    _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Concat(_timeStamp.ToString("HH:mm:ss.fff"), _value);
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         #endregion
     }
 }
