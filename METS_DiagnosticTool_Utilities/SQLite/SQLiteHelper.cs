d7940a634936cec5a13cd79c77b06d2f9148f878
Michal Jazwinski <mail@mihow.pl>
2021-09-15 20:14:04 +0200

    0.0.0.1 -> working on dumping the table to CSV

--- METS_DiagnosticTool_Utilities/SQLite/SQLiteHelper.cs
+++ METS_DiagnosticTool_Utilities/SQLite/SQLiteHelper.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -73,6 +74,10 @@ namespace METS_DiagnosticTool_Utilities.SQLite
 
         public static void DeleteTable(string plcVariableAddress)
         {
+            // TESTING!
+            // Dump Table to CSV Before Deleting
+            DumpTableToCSV(@"C:\Users\MIHOW\source\repos\METS_DiagnosticTool\METS_DiagnosticTool_Core\bin\Release\test.csv", plcVariableAddress);
+
             using (IDbConnection cnn = new SQLiteConnection(@"Data Source = .\METSDiagnosticTool_DB.db"))
             {
                 // First check does the Table Exists if not Create it
@@ -80,6 +85,74 @@ namespace METS_DiagnosticTool_Utilities.SQLite
                 string _tableName = plcVariableAddress.ToUpper().Replace('.', '_').Replace("[", string.Empty).Replace("]", string.Empty);
                 string _query = string.Concat(string.Concat("DROP TABLE if exists ", _tableName));
                 cnn.Execute(_query);
+            }
+        }
+
+        public static void DumpTableToCSV(string fileName, string plcVariableAddress)
+        {
+            try
+            {
+                string _tableName = plcVariableAddress.ToUpper().Replace('.', '_').Replace("[", string.Empty).Replace("]", string.Empty);
+                var source = SourceData(string.Concat("SELECT * FROM ", _tableName), _tableName);
+
+                File.WriteAllLines(fileName, ToCsv(source));
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(Logger.logLevel.Error, string.Concat("Exception when trying to Dump Table to CSV ", ex.ToString()), Logger.logEvents.Blank);
+            }
+
+        }
+
+        private static IEnumerable<IDataRecord> SourceData(string sql, string tableName)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(@"Data Source = .\METSDiagnosticTool_DB.db"))
+            {
+                cnn.Open();
+                
+                IEnumerable<PLCVariableDataModel> output = cnn.Query<PLCVariableDataModel>(string.Concat("SELECT 1 FROM sqlite_master WHERE type='table' AND name='", tableName, "'"), new DynamicParameters());
+                
+                if(output.Count() > 0)
+                {
+                    using (SQLiteCommand q = new SQLiteCommand(sql, (SQLiteConnection)cnn))
+                    {
+                        using (var reader = q.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                //TODO: you may want to add additional conditions here
+
+                                yield return reader;
+                            }
+                        }
+                    }
+                }
+
+                cnn.Close();
+            }
+        }
+
+        private static IEnumerable<string> ToCsv(IEnumerable<IDataRecord> data)
+        {
+            foreach (IDataRecord record in data)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < record.FieldCount; ++i)
+                {
+                    // TO DO GET ALL THE RECORDS in correct order
+                    string chunk = Convert.ToString(record.GetValue(0));
+
+                    if (i > 0)
+                        sb.Append(',');
+
+                    if (chunk.Contains(',') || chunk.Contains(';'))
+                        chunk = "\"" + chunk.Replace("\"", "\"\"") + "\"";
+
+                    sb.Append(chunk);
+                }
+
+                yield return sb.ToString();
             }
         }
     }
